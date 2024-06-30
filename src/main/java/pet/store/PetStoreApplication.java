04991ca82a83eeae211022938854f59b478f5d41
unknown <samuel.ajao@gmail.com>
2024-06-29 17:59:36 -0700

    Created Pet Store REST API project. Entities,JPA, and Spring Application initialized correctly.

---
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,21 @@
+/*
+// Program: Promineo Tech Backend Java Back End Development Course
+// Author:  Samuel Ajao
+// Subject:  Spring Boot API Coding Assignment - Pet Store
+// Create Date: June 29, 2024
+//
+*/
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+
+	}
+
+}
